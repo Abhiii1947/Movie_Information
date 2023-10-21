4d2d629f1d33f7ca697eb14ec7773186282635a2
Abhiii1947 <116731373+Abhiii1947@users.noreply.github.com>
2023-10-21 16:19:54 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,14 @@
+import Movies from './Movies';
+import Search from "./Search";
+
+const Home = () => {
+  // const name = useContext(AppContext);
+  return  (
+    <>
+      <Search />
+      <Movies />
+    </>
+  );
+};
+
+export default Home;
